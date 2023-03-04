1d403076be740a9a082040709376185978d4472d
AKhart1 <S200823@sggw.edu.pl>
2023-03-04 14:14:46 +0100

    Add project files.

---
+++ OpenAI_ImgGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpenAI_ImgGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
