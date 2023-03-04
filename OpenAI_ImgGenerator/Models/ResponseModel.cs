eedcd98007e69d5a3aaa3e2add0d1eea3f279443
AKhart1 <S200823@sggw.edu.pl>
2023-03-04 14:23:52 +0100

    Added Response Model

---
+++ OpenAI_ImgGenerator/Models/ResponseModel.cs
@@ -0,0 +1,30 @@
+﻿namespace DALLE_webapp.Models
+{
+    public class ResponseModel
+    {
+        public class Input
+        {
+            public string? prompt { get; set; }
+            public short? n { get; set; }
+            public string? size { get; set; }
+        }
+
+        public class Link
+        {
+            public string? url { get; set; }
+        }
+
+        public class ReponseModel
+        {
+            public long created { get; set; }
+            public List<Link>? data { get; set; }
+        }
+    }
+
+    public class VariationInput
+    {
+        public string? image { get; set; }
+        public short? n { get; set; }
+        public string? size { get; set; }
+    }
+}
