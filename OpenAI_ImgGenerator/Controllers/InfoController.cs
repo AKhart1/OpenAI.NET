7fb4db60405d70212bfd2a74c8f0cda266481462
AKhart1 <S200823@sggw.edu.pl>
2023-04-25 19:44:13 +0200

    connect pages to nav bar

---
+++ OpenAI_ImgGenerator/Controllers/InfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenAI_ImgGenerator.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- OpenAI_ImgGenerator/Program.cs
+++ OpenAI_ImgGenerator/Program.cs
@@ -24,4 +24,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Info}/{action=About}");
+
 app.Run();
