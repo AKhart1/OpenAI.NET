8d36b9f66d25ee84a1a9e5977e7039538443b029
AKhart1 <S200823@sggw.edu.pl>
2023-04-26 15:40:04 +0200

    Refactoring

---
+++ OpenAI_ImgGenerator/Views/Info/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenAI_ImgGenerator.Views.Info
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
